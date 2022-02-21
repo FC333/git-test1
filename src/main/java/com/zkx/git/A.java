d1615ec2b21a2e8660e588f0c3b462b04b674ce3
FC333 <2990299255@qq.com>
2022-02-21 16:03:16 +0800

    commit Time

--- src/main/java/com/zkx/git/A.java
+++ src/main/java/com/zkx/git/A.java
@@ -1,9 +1,5 @@
 package com.zkx.git;
 
-/**
- * @author Ori
- * @date 2022/2/21-15:46
- */
 public class A {
     public static void main(String[] args) {
         System.out.println("AA");
--- src/main/java/com/zkx/git/B.java
+++ src/main/java/com/zkx/git/B.java
@@ -1,9 +1,5 @@
 package com.zkx.git;
 
-/**
- * @author Ori
- * @date 2022/2/21-15:49
- */
 public class B {
     public static void main(String[] args) {
         System.out.println("B");
