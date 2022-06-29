04b96c5906bc4623ef2582dbed35fe69cabade22
A0505 <A0505@163.com>
2022-06-29 12:40:02 +0800

    629 first commit

--- src/main/java/com/zkx/git/A.java
+++ src/main/java/com/zkx/git/A.java
@@ -4,5 +4,6 @@ public class A {
     public static void main(String[] args) {
         System.out.println("AA");
         System.out.println("AA");
+        System.out.println("0629");
     }
 }
---
+++ src/main/java/com/zkx/git/B.java
@@ -0,0 +1,7 @@
+package com.zkx.git;
+
+public class B {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
